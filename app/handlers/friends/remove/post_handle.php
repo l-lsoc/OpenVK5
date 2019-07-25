7bf198a6d3561a661e280a50a453fbcbdcf5fe38
hioi <kitsuruko@gmail.com>
2019-07-25 22:53:58 +0300

    Fix group bug | Fixes #3

--- app/handlers/friends/remove/post_handle.php
+++ app/handlers/friends/remove/post_handle.php
@@ -5,12 +5,22 @@ return (function($args) {
 
     $database  = open_database();
     $relations = $database->table("followers");
+    $groups    = $database->table("groups");
     $params    = [
         "follower" => $args["__user__"]->id,
         "target" => $args["id"],
     ];
 
     $relations->where($params)->delete();
+
+    if($args["id"] < 0) {
+        $group = $groups->get(abs($args["id"]));
+        $admins = array_values(json_decode($group->coadmins, true));
+        if(in_array($args["__user__"]->id, $admins)) {
+            $admins = array_diff($admins, [$args["__user__"]->id]);
+            $groups->where("id", $group->id)->update(["coadmins" => json_encode($admins)]);
+        }
+    }
 
     header("HTTP/1.1 302 Found");
     header("Location: ".SOCN_CONFIG["URL"]."/?/".($args["id"] < 0 ? "public" : "user")."&id=".abs($args["id"]));
